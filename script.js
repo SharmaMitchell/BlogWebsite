93543f139d89a930ea4414ac865edb1c36e35802
Mitchell <90817905+SharmaMitchell@users.noreply.github.com>
2022-02-01 21:38:45 +0000

    Updated header & footer color (black), updated footer icon colors (white), removed hamburger pulse animation

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ hamburger.addEventListener('click', () => {
 
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
-	if (scroll_position > 250) {
-		header.style.backgroundColor = '#29323c';
+	if (scroll_position > 100) {
+		header.style.backgroundColor = '#212121';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
